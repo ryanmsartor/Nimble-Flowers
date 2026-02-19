11b72d253fb79c723ed97705d7e4f18897a56464
Ryan Sartor <ryanmsartor@gmail.com>
2026-02-19 10:22:46 -0600

    cards through wisteria

--- nf_cards.nim
+++ nf_cards.nim
@@ -40,12 +40,114 @@ const
         hachihachi_value: 1,
         hachi_value: 10
     )
-    plum_warbler = Card(
+
+    plum_animal* = Card(
         full_name: "Plum Blossoms with Bush Warbler",
         alt_names: @[""],
         standard_suit: 2,
         mushi_suit: 2,
         nagoya_suit: 12,
         hachihachi_value: 10,
+        hachi_value: 10
+    )
+    plum_ribbon* = Card(
+        full_name: "Plum Blossoms with Poetry Ribbon",
+        alt_names: @[""],
+        standard_suit: 2,
+        mushi_suit: 2,
+        nagoya_suit: 12,
+        hachihachi_value: 5,
+        hachi_value: 1
+    )
+    plum_chaff_1* = Card(
+        full_name: "Plum Blossom chaff",
+        alt_names: @[""],
+        standard_suit: 2,
+        mushi_suit: 2,
+        nagoya_suit: 12,
+        hachihachi_value: 1,
+        hachi_value: 10
+    )
+    plum_chaff_2* = Card(
+        full_name: "Plum Blossom chaff",
+        alt_names: @[""],
+        standard_suit: 2,
+        mushi_suit: 2,
+        nagoya_suit: 12,
+        hachihachi_value: 1,
+        hachi_value: 10
+    )
+
+    cherry_bright* = Card(
+        full_name: "Cherry Blossoms with Curtain",
+        alt_names: @[""],
+        standard_suit: 3,
+        mushi_suit: 3, 
+        nagoya_suit: 3, 
+        hachihachi_value: 20,
+        hachi_value: 10
+    )
+    cherry_ribbon* = Card(
+        full_name: "Cherry Blossoms with Poetry Ribbon",
+        alt_names: @[""],
+        standard_suit: 3,
+        mushi_suit: 3,
+        nagoya_suit: 3,
+        hachihachi_value: 5,
+        hachi_value: 1
+    )
+    cherry_chaff_1* = Card(
+        full_name: "Cherry Blossom chaff",
+        alt_names: @[""],
+        standard_suit: 3,
+        mushi_suit: 3,
+        nagoya_suit: 3,
+        hachihachi_value: 1,
+        hachi_value: 10
+    )
+    cherry_chaff_2* = Card(
+        full_name: "Cherry Blossom chaff",
+        alt_names: @[""],
+        standard_suit: 3,
+        mushi_suit: 3,
+        nagoya_suit: 3,
+        hachihachi_value: 1,
+        hachi_value: 10
+    )
+
+    wisteria_animal* = Card(
+        full_name: "Wisteria with Cuckoo",
+        alt_names: @[""],
+        standard_suit: 4,
+        mushi_suit: 4,
+        nagoya_suit: 4,
+        hachihachi_value: 10,
+        hachi_value: 10
+    )
+    wisteria_ribbon* = Card(
+        full_name: "Wisteria with Ribbon",
+        alt_names: @[""],
+        standard_suit: 4,
+        mushi_suit: 4,
+        nagoya_suit: 4,
+        hachihachi_value: 5,
+        hachi_value: 1
+    )
+    wisteria_chaff_1* = Card(
+        full_name: "Wisteria chaff",
+        alt_names: @[""],
+        standard_suit: 4,
+        mushi_suit: 4,
+        nagoya_suit: 4,
+        hachihachi_value: 1,
+        hachi_value: 10
+    )
+    wisteria_chaff_2* = Card(
+        full_name: "Wisteria chaff",
+        alt_names: @[""],
+        standard_suit: 4,
+        mushi_suit: 4,
+        nagoya_suit: 4,
+        hachihachi_value: 1,
         hachi_value: 10
     )
